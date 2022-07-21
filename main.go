1e779fd70e5c793f02caa7c398158d444d38f014
fajaranggara <fajartrianggara1@gmail.com>
2022-07-21 17:29:13 +0700

    add README

--- main.go
+++ main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"log"
 	"user-api/config"
 	"user-api/routes"
+
+	"github.com/joho/godotenv"
 )
 func main() {
+  // for load godotenv
+  err := godotenv.Load()
+  if err != nil {
+      log.Fatal("Error loading .env file")
+  }
+
+  //database connection
   db := config.ConnectDataBase()
   sqlDB, _ := db.DB()
   defer sqlDB.Close()
 
+  //router
   r := routes.SetupRouter(db)
-  r.Run()
+  r.Run("localhost:8080")
 }
